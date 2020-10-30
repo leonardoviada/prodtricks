79bf8478042bd0fd7895ba2ebc630fd029c9d451
leonardoviada <leo@revodigital.it>
2020-10-30 19:52:55 +0100

    First Code Draft

---
+++ src/MyApp.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyApp(): JSX.Element {
+  return (
+    <>
+      <div>
+        <h1>Hello React</h1>
+      </div>
+      <div>
+        <li>Leonardo</li>
+        <li>Viada</li>
+      </div>
+    </>
+  );
+}
+
+export default MyApp;
